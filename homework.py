7cba67beeaa0e4c378bcf7c4cec63d188095027b
Алексей <d.harlan@yandex.ru>
2021-11-19 19:09:42 +0300

    текст комментария

--- homework.py
+++ homework.py
@@ -146,6 +146,12 @@ if __name__ == "__main__":
         ('WLK', [9000, 1, 75, 180]),
     ]
 
+    packages = [
+        ('SWM', [720, 1, 80, 25, 40]),
+        ('RUN', [15000, 1, 75]),
+        ('WLK', [9000, 1, 75, 180]),
+    ]
+
 for workout_type, data in packages:
     training = read_package(workout_type, data)
     main(training)
