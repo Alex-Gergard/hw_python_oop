b56c9d3414cdf2a316e6416af61bb808c88a4257
Алексей <d.harlan@yandex.ru>
2021-11-27 22:45:28 +0300

    текст комментария

--- homework.py
+++ homework.py
@@ -144,12 +144,11 @@ def get_start_message():
 
     if __name__ == '__main__':
         packages = [
-        ('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),
-    ]
+            ('SWM', [720, 1, 80, 25, 40]),
+            ('RUN', [15000, 1, 75]),
+            ('WLK', [9000, 1, 75, 180]),
+        ]
 
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
-        
