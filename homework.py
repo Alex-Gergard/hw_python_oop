eecf37845ca483fa4ef4999cae12f956a25eba5a
Alexey <Alexey.bely.1982@yandex.ru>
2022-02-02 17:08:05 +0700

    work

--- homework.py
+++ homework.py
@@ -1,6 +1,8 @@
+from asyncio.windows_events import NULL
+
+
 class Training:
-    """Basic class Training."""
-    COEFF_CONVERSION = 1000
+    """Basic class Training."""    
     LEN_STEP = 0.65
     M_IN_KM = 1000
     def __init__(self, action, duration, weight):
@@ -9,7 +11,7 @@ class Training:
         self.weight = weight        
     def get_distance(self):
         """The value of the distance."""
-        return (self.action*Training.LEN_STEP) / Training.COEFF_CONVERSION
+        return (self.action*Training.LEN_STEP) / Training.M_IN_KM
     def get_mean_speed(self):
         """Average speed value."""
         return self.get_distance() / self.duration
@@ -18,7 +20,9 @@ class Training:
         return 0
     def show_training_info(self):
         """Information message object."""
-        return InfoMessage
+        temp = __class__.__name__
+        return InfoMessage(self.__class__.__name__, self.duration,  self.get_distance(),
+                           self.get_mean_speed(), self.get_spent_calories())
 
 class Running(Training):
     """Running class."""   
@@ -43,9 +47,11 @@ class Swimming(Training):
         super().__init__(action, duration, weight)
         self.length_pool = length_pool
         self.count_pool = count_pool
+    def get_distance(self):        
+        return (self.action*Swimming.LEN_STEP) / Training.M_IN_KM    
     def get_mean_speed(self):
         return ((self.length_pool
-               *self.count_pool) / Training.COEFF_CONVERSION) / self.duration       
+               *self.count_pool) / Training.M_IN_KM) / self.duration       
     def get_spent_calories(self):        
         return (self.get_mean_speed()+1.1) * 2*self.weight
 
@@ -58,29 +64,37 @@ class InfoMessage:
     speed - средняя скорость движения во время движения
     calories - потраченные за время тренировки килокалории
     """
-    def __init__(self, training_type, duration, distance, speed, calories):
+    def __init__(self, training_type: str, duration, distance, speed, calories):
         self.training_type = training_type
         self.duration = duration
         self.distance = distance
         self.speed = speed
         self.calories = calories        
     def get_message(self):
-        return f'Тип тренировки: {self.training_type}; \
-                Длительность: {self.duration} ч.; \
-                Дистанция: {round(self.distance, 3)} км; \
-                Ср. скорость: {round(self.speed, 3)} км/ч; \
-                Потрачено ккал: {round(self.calories, 3)}.'
+        return (f'Тип тренировки: {self.training_type}; ' 
+               f'Длительность: {self.duration:.3f} ч.; ' 
+               f'Дистанция: {self.distance:.3f} км; '
+               f'Ср. скорость: {self.speed:.3f} км/ч; '
+               f'Потрачено ккал: {self.calories:.3f}.')
 
-def read_package():
-    get_start_message()    
-    user_input = input()
-    user_param = user_input.split()
-    count_param = int(len(user_param))    
-    if count_param == 3:
-        return Running(user_param[0], user_param[1], user_param[2])
+def read_package(type_training, param_training):
+    if type_training == 'RUN':
+        return Running(param_training[0], param_training[1], param_training[2])
+    elif type_training == 'WLK':    
+        return SportsWalking(param_training[0], param_training[1], param_training[2], param_training[3])
+    elif type_training == 'SWM':    
+        return Swimming(param_training[0], param_training[1], param_training[2], param_training[3], param_training[4])
+    else:
+        print ('Не известнный код тренировки')
+        return NULL
 
+def main(training_info) -> None:
+    info = training_info.show_training_info()                
+    print(info.get_message())
+    
+def test_prgm():
+    training_1 = read_package('SWM', [720, 1, 80, 25, 40])        
+    main(training_1)
 
-  
-
-
-
+test_prgm()
+i = 777   
